a61b820af745bbd6d959cc2822414bce8ef9c92d
Cosmin <cosmin.ciocan@savethechildren.org>
2022-06-27 19:14:43 +0300

    Lab 4

--- src/LogicalOperations.java
+++ src/LogicalOperations.java
@@ -28,4 +28,21 @@ public class LogicalOperations {
         }
         return "Not applicable";
     }
+
+    public void printToHundred(int x){
+        for (int i = x; i <= 100; i++){
+            System.out.println(i);
+        }
+    }
+
+    public double sumAndAverage(){
+        int sum=0;
+        for (int i = 1; i <= 100; i++){
+            sum+=i;
+            System.out.println(sum);
+        }
+        return sum / 100d;
+    }
+
+//    1,2,3,4,.....97,98,99,100
 }
---
+++ src/Main4.java
@@ -0,0 +1,26 @@
+public class Main4 {
+
+
+    public static void main(String[] args) {
+
+//        Given a number, while the number
+//        is equal to or lower than 100, print the number;
+        LogicalOperations op = new LogicalOperations();
+
+        int x = 12;
+
+//        op.printToHundred(x);
+
+
+//Write a program
+// called SumAndAverage to produce the
+// sum of 1, 2, 3, ..., to 100. Also
+// compute and display the average.
+
+        System.out.println(op.sumAndAverage());
+
+
+
+
+    }
+}
